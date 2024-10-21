de7bf4e44c6a895852b76a0fd1246befdb59ecd6
Dan SYLVAIN <dansylvain84@gmail.com>
2024-10-21 18:33:39 +0200

    request parsing bug fixed

--- src/kernel/Server.cpp
+++ src/kernel/Server.cpp
@@ -126,9 +126,10 @@ void Server::handleClientRequest(size_t i, ssize_t ret)
 	if (std::search(this->_clients[i].message.begin(),
 		this->_clients[i].message.end(), "\r\n\r\n",
 		"\r\n\r\n" + 4) != this->_clients[i].message.end())
-	{						
+	{
+		RequestParser parser;				
 		this->_parser.parse(this->_clients[i]);								
-		this->_parser.displayAttributes();	
+		this->_parser.displayParsingResult();	
 		this->_clients[i].message.clear();
 	}
 	this->_readBuffer.clear();
