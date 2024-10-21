b69aef8781a093b44494d56774556e00db705e2b
“maitreverge_42” <verge.f@gmail.com>
2024-10-21 10:48:39 +0200

    add pseudo code

--- src/build_response/masterBuilder.cpp
+++ src/build_response/masterBuilder.cpp
@@ -3,6 +3,19 @@
 // TODO : Decide with the execution part on how to encapsulate data input
 vector<char> masterBuilder( vector<char> &bodyInput, e_errorCodes errorCode, string& fileName ){
 
+	/*
+		Selon le code d'erreur recu par masterBuilder, cette fonction doit se charger
+		de faire un ifstream pour recuperer les pages HTML d'erreur correspondantes
+	*/
+
+	/*
+		Pseudo code :
+		if (error_code 400 ou 500 + pas de body)
+			=> chercher les pages HTML d'erreur correspondantes
+		if pas de code d'erreur
+			=> CODE_200
+		(potnetiellement voir si un code d'erreur nest pas fourni, mettre un code par default)
+	*/
 	// Builder class
 	buildResponse builder;
 	
