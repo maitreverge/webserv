d61123b50f79d37f491fbe91e3079d3a961adc00
Dan SYLVAIN <dansylvain84@gmail.com>
2024-10-21 10:17:31 +0200

    toto

--- src/RequestParser/RequestParser.cpp
+++ src/RequestParser/RequestParser.cpp
@@ -200,7 +200,6 @@ void RequestParser::displayAttributes() const
 
 void		RequestParser::displayHeaders() const
 {
-	Logger::getInstance().log("************ HELLO FROM LOGGER!!! ************", *this);
 	print("-------- HEADERS -------");
 	if (_Headers.Connection.length())
 		print("Connection: " + _Headers.Connection);
