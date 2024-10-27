21fe0eafc199a6ef788f9cc41c4d7f90d43d823f
Dan SYLVAIN <dansylvain84@gmail.com>
2024-10-27 18:43:23 +0100

    end of session

--- src/ConfigFileParser/ConfigFileParser.hpp
+++ src/ConfigFileParser/ConfigFileParser.hpp
@@ -9,6 +9,11 @@
 #include <vector>
 #include "tmpConfig.hpp"
 #include <cstdlib>
+
+typedef std::map<std::string, std::map<std::string, std::vector<std::string> > >::const_iterator catIt;
+typedef std::map<std::string, std::vector<std::string> >::const_iterator itemIt;
+typedef std::vector<std::string>::const_iterator valIt;
+typedef std::map<std::string, std::map<std::string, std::vector<std::string> > > Data;
 struct server
 {
 	std::string	host;
@@ -16,9 +21,6 @@ struct server
 	std::string	serverName;
 };
 
-typedef std::map<std::string, std::map<std::string, std::vector<std::string> > >::const_iterator catIt;
-typedef std::map<std::string, std::vector<std::string> >::const_iterator itemIt;
-typedef std::vector<std::string>::const_iterator valIt;
 class ConfigFileParser
 {
 	#ifdef UNIT_TEST
@@ -26,26 +28,26 @@ class ConfigFileParser
 	#else
 	private:
 	#endif
-		struct server _serverStruct[4];
-		std::map<std::string, std::map<std::string, std::vector<std::string> > > _data;
-		std::map<std::string, std::map<std::string, std::vector<std::string> > > data;
+		struct server	_serverStruct[4];
+		Data			_data;
 
-	void	print(std::string str);
-	void	trim(std::string& str);
-	void	printData(const std::map<std::string, std::map<std::string, std::vector<std::string> > >& data);
-	int		ignoreComents(std::string& line);
-	int		getCurrentCategory(std::string& line, std::string& currentCategory);
-	void	extractKeyValuePairs(std::string& line, std::string& currentCategory);
-	void	printServerData(const server serverStruct[], size_t size);
-	void	setConfigValue(catIt& catIt, itemIt& itemIt, valIt& valIt, Config& configStruct, const char str[], e_errorCodes e);
-	void	setConfigValue(catIt& catIt, itemIt& itemIt, valIt& valIt, std::string& field, const char str[], int& i);
-	void	setConfigValue(catIt& catIt, itemIt& itemIt, short& field, const char str[]);
-	void	setConfigValue(catIt& catIt, itemIt& itemIt, bool& field, const char str[]);
-	void	setConfigValue(catIt& catIt, itemIt& itemIt, valIt& valIt, std::vector<std::string> vec, const char str[]);
-	void	initializeServers(Config configStruct, int& i);
-	bool	isServerData(const std::string& category);
+		void	print(std::string str);
+		void	trim(std::string& str);
+		void	printData(const std::map<std::string, std::map<std::string, std::vector<std::string> > >& data);
+		int		ignoreComents(std::string& line);
+		int		getCurrentCategory(std::string& line, std::string& currentCategory);
+		void	extractKeyValuePairs(std::string& line, std::string& currentCategory);
+		void	printServerData(const server serverStruct[], size_t size);
+		void	setConfigValue(catIt& catIt, itemIt& itemIt, valIt& valIt, Config& configStruct, const char str[], e_errorCodes e);
+		void	setConfigValue(catIt& catIt, itemIt& itemIt, valIt& valIt, std::string& field, const char str[], int& i);
+		void	setConfigValue(catIt& catIt, itemIt& itemIt, short& field, const char str[]);
+		void	setConfigValue(catIt& catIt, itemIt& itemIt, bool& field, const char str[]);
+		void	setConfigValue(catIt& catIt, itemIt& itemIt, valIt& valIt, std::vector<std::string> vec, const char str[]);
+		void	initializeServers(Config configStruct, int& i);
+		bool	isServerData(const std::string& category);
+
 	public:
-	void	intializeConfigStruct(Config configStruct);
-	int		extractDataFromConfigFile(const std::string);
-	void	parseConfigFile(Config configStruct);
+		void	intializeConfigStruct(Config configStruct);
+		int		extractDataFromConfigFile(const std::string);
+		void	parseConfigFile(Config configStruct);
 };
