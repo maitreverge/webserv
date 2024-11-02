cc00f9f6ded490fb1c2c734ee8f7c4472855a466
sierra182 <sierra-echo@msn.com>
2024-11-02 12:55:41 +0100

    search hardresponse

---
+++ src/kernel/Cgi.cpp
@@ -0,0 +1,59 @@
+#include "Cgi.hpp"
+#include "Logger.hpp"
+
+void Cgi::launch()
+{           
+    socketpair(AF_UNIX, SOCK_STREAM, 0, this->fds);
+    pid_t pid = fork();
+    if (pid < 0)
+        Logger::getInstance().log(ERROR, "fork failed");
+    else if (pid == 0)
+    {
+       
+        dup2(this->fds[0], STDIN_FILENO); 
+        dup2(this->fds[0], STDOUT_FILENO); 
+        close(this->fds[0]);
+        close(this->fds[1]);
+        // int t[2] = {1,2, NULL}
+        // char *args[] = {NULL};
+        // char *envp[] = {NULL};
+        // execve("./cgi/a.out", args, envp);
+        chdir("./cgi");
+        std::string str("PATH_INFO=coucoucpathinfo");
+        char *env[] = 
+        {
+            const_cast<char *>(str.c_str()), NULL
+        };
+        execve("a.out", (char *[]){NULL}, env);       
+        std::cout << "execve fail" << std::endl;
+    } 
+    else
+    {                
+        close(this->fds[0]);
+
+        // char buff[150];
+        // char buff2[5] = {'s','a','l','u','t'};
+        // send(this->fds[1], buff2, sizeof(buff2), 0);
+        // ssize_t retp = recv(this->fds[1], buff, sizeof(buff), 0);
+        // for (ssize_t i = 0; i < retp; i++)
+            // std::cout << "." << buff[i];   
+    }    
+};
+
+void Cgi::getBody(Client & client)
+{
+    Logger::getInstance().log(INFO, "hello from parent");
+    
+    
+    // char buff[150];
+    // char buff2[5] = {'s','a','l','u','t'};
+    // send(this->fds[1], buff2, sizeof(buff2), 0);
+    client.messageSend.clear();
+    client.messageSend.resize(SEND_BUFF_SIZE);//!
+    ssize_t ret = recv(this->fds[1], client.messageSend.data(),
+        client.messageSend.size(), 0);
+    // for (ssize_t i = 0; i < ret; i++)
+    //     std::cout << "." << buff[i];
+    if (!ret)
+        close(this->fds[1]);
+};
--- src/kernel/Cgi.hpp
+++ src/kernel/Cgi.hpp
@@ -1,69 +1,15 @@
 #pragma once
 
 #include <unistd.h>
-#include <Logger.hpp>
+
+struct Client;
 
 class Cgi
 {
     int fds[2];
 
     public:
-        
-        Cgi(){};
-        void launch()
-        {           
-            socketpair(AF_UNIX, SOCK_STREAM, 0, this->fds);
-            pid_t pid = fork();
-            if (pid < 0)
-                Logger::getInstance().log(ERROR, "fork failed");
-            else if (pid == 0)
-            {
-                std::cout << "hello from child1" << std::endl;
-                dup2(this->fds[0], STDIN_FILENO); // met le fd 0 a l aplace de in
-                dup2(this->fds[0], STDOUT_FILENO); // met le fd 0 a l aplace de out
-                close(this->fds[0]);
-                close(this->fds[1]);
-                // int t[2] = {1,2, NULL}
-                // char *args[] = {NULL};
-                // char *envp[] = {NULL};
-                // execve("./cgi/a.out", args, envp);
-                chdir("./cgi");
-                std::string str("PATH_INFO=coucoucpathinfo");
-                char *env[] = 
-                {
-                    const_cast<char *>(str.c_str()), NULL
-                };
-                execve("a.out", (char *[]){NULL}, env);
-                // execve("./cgi/a.out", static_cast<char **>(NULL), static_cast<char **>(NULL));
-                std::cout << "execve fail" << std::endl;
-            } 
-            else
-            {                
-                close(this->fds[0]);
-
-                char buff[150];
-                char buff2[5] = {'s','a','l','u','t'};
-                send(this->fds[1], buff2, sizeof(buff2), 0);
-                ssize_t retp = recv(this->fds[1], buff, sizeof(buff), 0);
-                for (ssize_t i = 0; i < retp; i++)
-                    std::cout << "." << buff[i];
-                close(this->fds[1]);
-                // wait(NULL);
-            }    
-        };
-
-        void getBody(Client & client)
-        {
-            Logger::getInstance().log(INFO, "hello from parent");
-            
-         
-            // char buff[150];
-            // char buff2[5] = {'s','a','l','u','t'};
-            // send(this->fds[1], buff2, sizeof(buff2), 0);
-            ssize_t ret = recv(this->fds[1], client.messageSend.data(),
-                static_cast<std::streamsize>(client.messageSend.size()), 0);
-            for (ssize_t i = 0; i < retp; i++)
-                std::cout << "." << buff[i];
-            close(this->fds[1]);
-        };
+             
+        void launch();  
+        void getBody(Client & client);   
 };
