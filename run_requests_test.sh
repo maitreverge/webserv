309fac68e6e3c0a693895f04016c99127c22b65b
Dan Sylvain <dansylvain84@gmail.com>
2024-11-21 09:06:21 +0100

    added check_file && check_file formatting

--- run_requests_test.sh
+++ run_requests_test.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Codes de couleurs ANSI pour la colorisation
+RED='\033[31m'
+GREEN='\033[32m'
+BLUE='\033[34m'
+YELLOW='\033[33m'
+CYAN='\033[36m'
+NC='\033[0m' # No Color
+
 # Dossiers des fichiers
 CONFIG_DIR="Requests_Tester/config_files"
 REQUESTS_DIR="Requests_Tester/requests"
 EXPECTED_ANSWERS_DIR="Requests_Tester/expected_answers"
 ANSWERS_DIR="Requests_Tester/answers"
 DIFF_DIR="Requests_Tester/diff"
+
+# Fichier de vérification global
+CHECK_FILE="Requests_Tester/check_file.ans"
+> "$CHECK_FILE" # On réinitialise le fichier à chaque exécution
 
 # Création des répertoires de résultats et de différences s'ils n'existent pas
 mkdir -p "$ANSWERS_DIR"
@@ -16,16 +28,22 @@ for config_file in "$CONFIG_DIR"/*.ini; do
 	# Extraction du numéro de configuration (par exemple, config_1.ini -> 1)
 	config_number=$(basename "$config_file" .ini | sed 's/config_//')
 
-	echo "Running tests for config_$config_number..."
+	# Vérification et colorisation des résultats (conf file)
+	echo -e "${BLUE}=== Configuration : ${config_file}${NC}" >> "$CHECK_FILE"
+	echo >> "$CHECK_FILE"
+	echo -e "${YELLOW}$(cat "$config_file")${NC}" >> "$CHECK_FILE"
+	echo >> "$CHECK_FILE"
 
 	# Lancement de Webserv avec le fichier de configuration
 	./webserv "$config_file" > /dev/null 2>&1 &  # Lancement en arrière-plan de Webserv avec le fichier de configuration
 	webserv_pid=$! # Récupère le PID du processus Webserv
-	sleep 1 # Laisser un peu de temps pour que Webserv démarre
+	
+	#TODO&&&&&&&&&&&&&&& DELAY TO BE ADJUSTED? &&&&&&&&&&&&&&&&&&&&
+	sleep 0.2 # Laisser un peu de temps pour que Webserv démarre
+	#TODO&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 	# Boucle sur les fichiers de requêtes pour ce fichier de configuration
 	request_file="$REQUESTS_DIR/config_${config_number}_requests.txt"
-	echo "Testing $request_file..."
 
 	test_counter=1
 	# Lire les requêtes dans le fichier et envoyer chaque requête individuellement
@@ -44,15 +62,34 @@ for config_file in "$CONFIG_DIR"/*.ini; do
 		echo "$http_response" > "$response_file"
 		# Comparaison des réponses avec les résultats attendus
 		expected_answer_file="$EXPECTED_ANSWERS_DIR/config_${config_number}_test_${test_counter}_expected.txt"
-		echo "$http_response" > "$expected_answer_file"
-		diff_file="$DIFF_DIR/config_${config_number}_test_${test_counter}_diff.txt"
+		
+		
+		#TODO && UNCOMMENT TO FILL EXPECTED_ANSWER_FILES &&&&&&&&&&
+		# echo "$http_response" > "$expected_answer_file"
+		#TODO &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
+
+		# Vérification et colorisation des résultats
+		echo -e "${RED}--- Requête : $request${NC}" >> "$CHECK_FILE"
+		echo >> "$CHECK_FILE"
+		echo -e "${BLUE}--- Expected Answer :${NC}" >> "$CHECK_FILE"
+		echo -e "${GREEN}$(cat "$expected_answer_file")${NC}" >> "$CHECK_FILE"
+		echo >> "$CHECK_FILE"
+		echo -e "${BLUE}--- Actual Answer :${NC}" >> "$CHECK_FILE"
+		echo >> "$CHECK_FILE"
+		echo -e "${CYAN}$(cat "$response_file")${NC}" >> "$CHECK_FILE"
+		echo >> "$CHECK_FILE"
+		echo >> "$CHECK_FILE"
+
+		diff_file="$DIFF_DIR/config_${config_number}_test_${test_counter}.diff"
+		
 		if diff -q "$response_file" "$expected_answer_file" > /dev/null; then
-			echo "Test $test_counter for config_$config_number: ✅ Success"
+			echo "config_$config_number, test $test_counter : $request ✅ Success"
 		else
-			echo "Test $test_counter for config_$config_number: ❌ Failure"
+			echo "config_$config_number, test $test_counter : $request ❌ Failure"
 			# Enregistrer le diff dans un fichier
 			diff "$response_file" "$expected_answer_file" > "$diff_file"
+			cat "$diff_file"
 		fi
 		test_counter=$((test_counter + 1))
 	done < "$request_file"
@@ -60,9 +97,7 @@ for config_file in "$CONFIG_DIR"/*.ini; do
 	# Arrêter Webserv une fois tous les tests effectués
 	kill "$webserv_pid"
 	wait "$webserv_pid" 2>/dev/null || {
-		echo "Webserv did not terminate properly, forcing kill."
 		kill -9 "$webserv_pid" 2>/dev/null
 	}
 
-	echo "Tests for config_$config_number completed."
 done
