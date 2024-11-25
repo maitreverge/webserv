5b5bfa16cefc4a81d177aca778baf297511171f8
Dan Sylvain <dansylvain84@gmail.com>
2024-11-25 10:38:28 +0100

    updated create files srcript

--- Requests_Tester/creation_files_script.sh
+++ Requests_Tester/creation_files_script.sh
@@ -9,18 +9,21 @@ cd Requests_Tester/delete_folder
 mkdir -p folder_1
 
 touch file_{1..2}.txt
-chmod 644 *
+chmod 755 *
 
 # Write some content in the file for getting them back
 echo "This is file_1.txt content" > file_1.txt
 echo "This is file_2.txt content" > file_2.txt
 
+echo "I am here: $PWD"
+echo "that's what I see:"
+ls
 # Remove write access to file_1.txt only
 chmod -w file_1.txt
 
 cd folder_1
 touch hello.txt
-chmod 644 *
+chmod 755 *
 echo "This is hello.txt content" > file_2.txt
 
 # Back to root
