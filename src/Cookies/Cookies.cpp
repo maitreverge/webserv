27ab5deca07bbc73955a62888bb4cb1f2d61721c
Dan Sylvain <dsylvain@c2r6p8.42perpignan.fr>
2024-12-05 14:36:04 +0100

    Cookies cleaned

--- src/Cookies/Cookies.cpp
+++ src/Cookies/Cookies.cpp
@@ -9,25 +9,28 @@ void	Cookies::checkSessionCookie(Headers& _Headers, Server& server, std::string 
 {
 	if (_Headers.Cookie.find("sessionID") != _Headers.Cookie.end())
 	{
-		Logger::getInstance().log(INFO, "cookie sessionID found.");
+		Logger::getInstance().log(INFO, "Cookie sessionID found.");
 		std::string value = _Headers.Cookie.find("sessionID")->second;
 		if (server.UserSessions.find(value) != server.UserSessions.end())
 		{
 			_cookies["sessionID"] = value;
 			Logger::getInstance().log(INFO, "Active session found.");
 		}
-		else if (URI == "/connect?") //! this URI should be changed
+		else if (URI == "/connect?")
 		{
 			SessionData sessionData = {value};
 			server.UserSessions[value] = sessionData;
-			Logger::getInstance().log(INFO, "client sessionID token added to server");
+			Logger::getInstance().log(INFO, "Client sessionID token added to server");
 			_cookies["sessionID"] = value;
 		}
 		else
-		{
-			Logger::getInstance().log(INFO, "client sessionID token not valid");
-		}
+			Logger::getInstance().log(INFO, "Client sessionID token not valid");
 	}
 	else
-		Logger::getInstance().log(INFO, "client not connected");
+		Logger::getInstance().log(INFO, "Client not connected");
+}
+
+CookiesMap Cookies::getCookies() const
+{
+	return _cookies;
 }
