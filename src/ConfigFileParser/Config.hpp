61e6c5157594c53a8812fe5b9cd4b988f79fa443
Dan Sylvain <dsylvain@c2r6p8.42perpignan.fr>
2024-12-05 13:18:48 +0100

    refac config.hpp

--- src/ConfigFileParser/Config.hpp
+++ src/ConfigFileParser/Config.hpp
@@ -18,33 +18,28 @@ struct server
 	RoutesData routesData;
 };
  
-/**========================================================================
- *                             CONFIG
- *========================================================================**/
 struct Config
 {
-	bool							handleCookies;
-	int								index;
-	size_t							maxBodySize;
-	ssize_t							maxHeaderSize;
-	int								maxServerNbr;
-	int								timeoutCgi;
-	struct server					_serverStruct[8];
-	short int						maxClient;
-	std::vector<struct sockaddr_in> sockAddress;
-	std::vector<std::string>		indexFiles;
-	bool							listingDirectories;
-	map<e_errorCodes, string>		errorPaths;	
-	map<string, string>				redirection;
-	std::string						errorPagesPath;
-	RoutesData						routes;
-	
-	unsigned short					recv_buff_size;
-	unsigned short					send_buff_size;
+	bool								handleCookies;
+	int									index;
+	size_t								maxBodySize;
+	ssize_t								maxHeaderSize;
+	int									maxServerNbr;
+	int									timeoutCgi;
+	struct server						_serverStruct[8];
+	short int							maxClient;
+	std::vector<struct sockaddr_in> 	sockAddress;
+	std::vector<std::string>			indexFiles;
+	bool								listingDirectories;
+	map<e_errorCodes, string>			errorPaths;	
+	map<string, string>					redirection;
+	std::string							errorPagesPath;
+	RoutesData							routes;
+	unsigned short						recv_buff_size;
+	unsigned short						send_buff_size;
 
 	Config();
 	Config(char* path);
-	void	initializeServers();
 	void	initializeServer(uint16_t port, std::vector<sockaddr_in>& sockAddress);
 	void	intitializeVars();
 };
