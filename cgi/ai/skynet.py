4aa8c5b8e9978fe11ec8a4a026e407c6b1021f27
sierra182 <sierra-echo@msn.com>
2024-12-07 16:48:38 +0100

    handle bad status cgi

--- cgi/ai/skynet.py
+++ cgi/ai/skynet.py
@@ -6,9 +6,9 @@ with open("skynet.html", 'r') as html:
     output = html.read()
 # while True:
 #     ...
-   
+#    exit(1)
 content_length = len(output.encode('utf-8'))
-print("TTP/1.1 200 OK")
+print("HTTP/1.1 200 OK")
 print("Content-Type: text/html; charset=utf-8")
 print(f"Content-Length: {content_length}")
 print()
