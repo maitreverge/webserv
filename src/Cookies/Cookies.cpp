8a39d5112aea1ec9cf5fdd8432915fc66c244241
Dan Sylvain <dsylvain@c2r6p8.42perpignan.fr>
2024-12-08 15:50:31 +0100

    cookie log conditionnaly ouput

--- src/Cookies/Cookies.cpp
+++ src/Cookies/Cookies.cpp
@@ -5,29 +5,29 @@
 Cookies::Cookies()
 {}
 
-void	Cookies::checkSessionCookie(Headers& _Headers, Server& server, std::string URI)
+void	Cookies::checkSessionCookie(Headers& _Headers, Server& server, std::string URI, Client& client)
 {
 	if (_Headers.Cookie.find("sessionID") != _Headers.Cookie.end())
 	{
-		Logger::getInstance().log(INFO, "Cookie sessionID found.");
+		Logger::getInstance().log(INFO, "Cookie sessionID found.", client);
 		std::string value = _Headers.Cookie.find("sessionID")->second;
 		if (server.UserSessions.find(value) != server.UserSessions.end())
 		{
 			_cookies["sessionID"] = value;
-			Logger::getInstance().log(INFO, "Active session found.");
+			Logger::getInstance().log(INFO, "Client connected (Active session found).", client);
 		}
 		else if (URI == "/connect")
 		{
 			SessionData sessionData = {value};
 			server.UserSessions[value] = sessionData;
-			Logger::getInstance().log(INFO, "Client sessionID token added to server");
+			Logger::getInstance().log(INFO, "Client connected (Client sessionID token added to server)", client);
 			_cookies["sessionID"] = value;
 		}
 		else
-			Logger::getInstance().log(INFO, "Client sessionID token not valid");
+			Logger::getInstance().log(INFO, "Client not connected: Client sessionID token not valid", client);
 	}
-	else
-		Logger::getInstance().log(INFO, "Client not connected");
+	else if (client.conf->handleCookies)
+		Logger::getInstance().log(INFO, "Client not connected", client);
 }
 
 CookiesMap Cookies::getCookies() const
