8d8d7d9c5c8b60e9d8d543dbb1557b7e150b0bb5
“maitreverge_42” <verge.f@gmail.com>
2024-12-10 13:25:58 +0100

    DELETE block done

--- _websites/correction/delete_ok/file_creation.sh
+++ _websites/correction/delete_ok/file_creation.sh
@@ -3,6 +3,7 @@
 echo "Creating ressources to delete"
 touch file1.json
 touch file2.txt
-chmod -r index.html
+touch impossible_to_delete.txt
+chmod 000 impossible_to_delete.txt
 cp ../../ressources_to_upload/cat_weird.jpg .
 echo "Ressources deleted"
