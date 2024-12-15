5d3f96fb9417d681a4db2802685d4d9c2e7e99c4
Dan Sylvain <dsylvain@c2r6p8.42perpignan.fr>
2024-12-15 17:55:07 +0100

    username added to userSession struct

--- src/Cookies/Cookies.cpp
+++ src/Cookies/Cookies.cpp
@@ -5,7 +5,21 @@
 Cookies::Cookies()
 {}
 
-void	Cookies::checkSessionCookie(Headers& _Headers, Server& server, std::string URI, Client& client)
+std::string extractUsername(const std::string& URI)
+{
+    size_t pos = URI.find("name=");
+    if (pos != std::string::npos)
+    {
+        size_t start = pos + 5; // 5 = longueur de "username="
+        size_t end = URI.find("&", start); // Recherche la fin du paramètre
+        if (end == std::string::npos)
+            end = URI.length();
+        return URI.substr(start, end - start);
+    }
+    return ""; // Retourne une chaîne vide si username n'existe pas
+}
+
+void	Cookies::checkSessionCookie(Headers& _Headers, Server& server, std::string& URI, Client& client)
 {
 	if (_Headers.Cookie.find("sessionID") != _Headers.Cookie.end())
 	{
@@ -16,12 +30,17 @@ void	Cookies::checkSessionCookie(Headers& _Headers, Server& server, std::string 
 			_cookies["sessionID"] = value;
 			Logger::getInstance().log(INFO, "Client Connected (Active session found).", client, L_VRB);
 		}
-		else if (URI == "/connect")
+		else if (URI.find("connect") != std::string::npos)
 		{
-			SessionData sessionData = {value, "tartampion"};
-			server.UserSessions[value] = sessionData;
+			std::string userName = extractUsername(URI);
+			SessionData sessionData = {value, userName};
+			client._server->UserSessions[value] = sessionData;
+			printColor(RED, "****************************************");
+			printColor(RED, client._server->UserSessions[value].userName);
+			printColor(RED, "****************************************");
 			Logger::getInstance().log(INFO, "Client Connected (Client sessionID token added to server)", client, L_VRB);
 			_cookies["sessionID"] = value;
+			URI = "/connect";
 		}
 		else
 			Logger::getInstance().log(INFO, "Client Not Connected: Client sessionID token not valid", client, L_VRB);
